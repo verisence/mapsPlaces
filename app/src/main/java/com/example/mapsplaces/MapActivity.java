12b3eb3a1440c548022bbbf36d9ba5e1f2db9f72
Phil Kipkemboi <kipkemboiphil@gmail.com>
2019-03-19 22:53:51 +0300

    Request for permissions before initislizing the map

--- app/src/main/java/com/example/mapsplaces/MapActivity.java
+++ app/src/main/java/com/example/mapsplaces/MapActivity.java
@@ -1,17 +1,58 @@
 package com.example.mapsplaces;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 
 public class MapActivity extends AppCompatActivity {
 
     private static final String TAG = "MapActivity";
 
+    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 123;
+
+    //vars
+    private Boolean mLocationPermissionGranted = false;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
         
+    }
+    private void getLocationPermission(){
+        String[] permissions = {Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION};
+
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                    COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            }else{
+                ActivityCompat.requestPermissions(this,
+                        permissions,
+                        LOCATION_PERMISSION_REQUEST_CODE);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+            mLocationPermissionGranted =false;
+
+            switch (requestCode){
+                case LOCATION_PERMISSION_REQUEST_CODE:{
+                    if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                        mLocationPermissionGranted = true;
+                    }
+                }
+            }
     }
 }
