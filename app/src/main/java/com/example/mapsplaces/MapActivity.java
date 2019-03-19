44a6e9b138d5bbcb17ec5164e2be538ee6eff606
Phil Kipkemboi <kipkemboiphil@gmail.com>
2019-03-19 20:43:46 +0300

    Add basic world map

---
+++ app/src/main/java/com/example/mapsplaces/MapActivity.java
@@ -0,0 +1,17 @@
+package com.example.mapsplaces;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class MapActivity extends AppCompatActivity {
+
+    private static final String TAG = "MapActivity";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+        
+    }
+}
